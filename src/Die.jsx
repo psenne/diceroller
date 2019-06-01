106929d94040286d256cfb156ed00c209b3ebc87
MJP <psenne@mjparker.com>
2019-05-31 22:14:41 -0400

    separated out dice for each roll

--- src/Die.jsx
+++ src/Die.jsx
@@ -6,10 +6,15 @@ const Box = styled.div`
     padding: 0.5rem;
 `;
 
+const DieOutline = styled.div`
+    border: 0;
+`;
+
 export default function Die({ dicetype }) {
     const [number, setnumber] = useState(1);
     const [mod, setmod] = useState(0);
-    const [total, settotal] = useState([]);
+    const [total, settotal] = useState(0);
+    const [dice, setdice] = useState([]);
 
     const UpdateNumber = ev => {
         setnumber(ev.currentTarget.value);
@@ -21,22 +26,23 @@ export default function Die({ dicetype }) {
 
     const Roll = () => {
         var rolls = 0;
-        var arrTotal = [...total];
+        // var arrTotal = [...total];
+        var arrTotal = [];
 
         for (var i = 0; i < parseInt(number); i++) {
-            rolls = rolls + Math.floor(Math.random() * parseInt(dicetype)) + 1;
+            var roll = Math.floor(Math.random() * parseInt(dicetype)) + 1;
+            rolls = rolls + roll;
+            arrTotal.push(roll);
         }
-
-        arrTotal.push(rolls + parseInt(mod));
-
-        settotal(arrTotal);
+        setdice(arrTotal);
+        settotal(rolls);
     };
 
     return (
         <Box>
             <div>
-                <input type="number" onChange={UpdateNumber} value={number} min="1" />d{dicetype} + <input type="number" onChange={UpdateMod} value={mod} min="0" />
-                <button onClick={Roll}>Roll</button> {total.join(", ")}
+                <input type="number" onChange={UpdateNumber} value={number} min="1" />d{dicetype}
+                <button onClick={Roll}>Roll</button> {dice.join(" + ")} {dice.length > 1 ? " = " + total : ""}
             </div>
         </Box>
     );
